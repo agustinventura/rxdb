b587be78aba492b2213782c7c54993789a0770f0
Agustín Ventura <agustinventura@gmail.com>
2019-03-17 19:57:06 +0100

    initial commit with spring data rest access

---
+++ src/main/java/com/digitalsingular/rxdb/enclosure/EnclosureRepository.java
@@ -0,0 +1,7 @@
+package com.digitalsingular.rxdb.enclosure;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EnclosureRepository extends PagingAndSortingRepository<Enclosure, Integer>{
+
+}
---
+++ src/test/java/com/digitalsingular/rxdb/RxdbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitalsingular.rxdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RxdbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
