8d6fb466f8fd90f7d44721f57a35d8730c7cc73d
Agustín Ventura <agustinventura@gmail.com>
2019-04-03 21:35:05 +0200

    Adds microtest PoC

--- src/test/java/com/digitalsingular/rxdb/RxdbApplicationTests.java
+++ src/test/java/com/digitalsingular/rxdb/RxdbApplicationTests.java
@@ -2,15 +2,25 @@ package com.digitalsingular.rxdb;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.digitalsingular.rxdb.enclosure.EnclosureRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RxdbApplicationTests {
 
+	@Autowired
+	private EnclosureRepository repository;
+
 	@Test
-	public void contextLoads() {
-	}
+    public void someBenchmarkMethod() {
+        long startTime = System.currentTimeMillis();
+        repository.findByPlot(1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Ha tardado " + (endTime - startTime));
+    }
 
 }
