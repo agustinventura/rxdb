7d677b5626dfc936181ba66d7ef75b3cd60c793a
Agustín Ventura <agustinventura@gmail.com>
2022-06-28 20:46:48 +0200

    Configures database using properties

--- src/main/java/com/digitalsingular/rxdb/DataBaseConfiguration.java
+++ src/main/java/com/digitalsingular/rxdb/DataBaseConfiguration.java
@@ -1,28 +1,11 @@
 package com.digitalsingular.rxdb;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
-import io.r2dbc.spi.ConnectionFactory;
 
 @Configuration
 @EnableR2dbcRepositories
-public class DataBaseConfiguration extends AbstractR2dbcConfiguration {
+public class DataBaseConfiguration {
 
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        PostgresqlConnectionConfiguration config =
-                PostgresqlConnectionConfiguration.builder()
-                        .host("localhost")
-                        .database("sigpac")
-                        .username("sigpac")
-                        .password("sigpac")
-                        .build();
-        return new PostgresqlConnectionFactory(config);
-
-    }
 }
